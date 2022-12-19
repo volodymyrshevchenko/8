d213ae6aabc072a8793a4b06bd431e41a1e83b10
volodymyrshevchenko <Chief_94@ukr.net>
2022-12-19 14:46:10 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String badNumber4 ="4";
+        String badNumber9 ="9";
+        int name = 0;
+
+        for (int i = 0; i<= 100; i++){
+            name++;
+            if (!(String.valueOf(name).contains(badNumber4)  || String.valueOf(name).contains(badNumber9))) {
+                System.out.println(name);
+
+            }
+            {
+
+            }
+                }
+
+            }
+
+            }
